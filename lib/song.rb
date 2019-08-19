9d883c7932982cee15d4a01f5835bf1e24a52a1d
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-19 16:14:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,11 @@ class Song
 def self.all 
   @@all 
 end 
-
+  def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
+  end
 end 
